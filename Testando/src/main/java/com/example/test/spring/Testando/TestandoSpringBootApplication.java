4724ba06179c498d29bfe2befe3dfa887c524f88
thiago-miralha <thiagomessiasmiralha@gmail.com>
2026-02-21 21:16:53 -0400

    Hello Word no spring boot

---
+++ Testando/src/main/java/com/example/test/spring/Testando/TestandoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.spring.Testando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoSpringBootApplication.class, args);
+	}
+
+}
---
+++ Testando/src/test/java/com/example/test/spring/Testando/TestandoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.spring.Testando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestandoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
