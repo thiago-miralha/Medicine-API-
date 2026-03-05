2b7a05a5160a412b0068ed7864cc409d931ea666
thiago-miralha <thiagomessiasmiralha@gmail.com>
2026-03-04 20:34:46 -0400

    tratando MethodArgumentNotValidException - erro 400

--- Testando/src/main/java/com/example/test/spring/Testando/remedio/exception/GlobalExceptionHandler.java
+++ Testando/src/main/java/com/example/test/spring/Testando/remedio/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.test.spring.testando.remedio.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,4 +18,20 @@ public class GlobalExceptionHandler {
 
     }
     
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratador400(MethodArgumentNotValidException ex){
+        var erros =ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+
+
+
+    }
+
+    public record DadosErros (String campo,String messagem) {
+
+        public DadosErros(FieldError erro){
+            this(erro.getField(),erro.getDefaultMessage());
+
+        }
+    }
 }
