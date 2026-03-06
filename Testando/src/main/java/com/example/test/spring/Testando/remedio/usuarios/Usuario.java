034bf265ef2b02f22dc817b6284eb2c631673cd3
thiago-miralha <thiagomessiasmiralha@gmail.com>
2026-03-05 20:18:39 -0400

    criando a entity usuario

---
+++ Testando/src/main/java/com/example/test/spring/Testando/remedio/usuarios/Usuario.java
@@ -0,0 +1,29 @@
+package com.example.test.spring.testando.remedio.usuarios;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "Usuario")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
